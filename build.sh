4a2d065df65513f0fc7edbca582593290a521ccb
Xi Ruoyao <xry111@mengyan1223.wang>
2021-07-05 00:12:08 +0800

    remove obselete file

--- build.sh
+++
@@ -1,9 +0,0 @@
-make -C code                \
-	hw.out                  \
-	mod.out                 \
-	mod_bad.out             \
-	strlen-1.out            \
-	strlen-2.out            \
-&&
-
-latexmk --lualatex beamer
